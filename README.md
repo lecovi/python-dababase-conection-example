824f3ddf07d56e3fb3b9ba3479cbc60b2bdf901c
lecovi <leandro.colombo@mercadolibre.com>
2022-09-24 10:30:06 -0300

    Example postgres connection

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# python-dababase-conection-example
+# Python Dababase Connection Example
+
 A Simple repository to show how to connect to a DB using Python.
+
+## Python DB API
+
+1. Run PostgreSQL docker container
+2. Read python source code
+3. Execute and test!
+
+### Run DB
+
+Run a Postgres docker container with the following command:
+
+```bash
+docker run \
+  --name db \  # The name of the database container
+  -e POSTGRES_PASSWORD=mysecretpassword \  # The DB superuser password
+  -p 5432:5432  # The DB exposed port
+  -d postgres:14-alpine
+```
+
+Now your DB is running. 
+You can check with a database tool like [DBeaver](https://dbeaver.io/). Download it
+and test your connection.
+
+![DBeaver Connection Example](/assets/img/dbeaver-connection-example.png)
+
+### Run Python Code
+
+Create a virtualenv and install the Postgres adapter
+
+```bash
+pip install "pyscopg[binary]"
+```
+
+Execute python code
+
+```bash
+python db_example.py
+```
+
+Use DBeaver to explore tables and data. And explore `psycopg` to interact with DB and 
+python code. 
---
+++ db_example.py
@@ -0,0 +1,40 @@
+import psycopg
+
+PG_USERNAME="postgres"
+PG_PASSWORD="mysecretpassword"
+PG_HOST="localhost"
+PG_DB_NAME="postgres"
+DB_URI=f"postgres://{PG_USERNAME}:{PG_PASSWORD}@{PG_HOST}/{PG_DB_NAME}"
+
+# Connect to an existing database
+with psycopg.connect(DB_URI) as conn:
+
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+
+        # Execute a command: this creates a new table
+        cur.execute("""
+            CREATE TABLE test (
+                id serial PRIMARY KEY,
+                num integer,
+                data text)
+            """)
+
+        # Pass data to fill a query placeholders and let Psycopg perform
+        # the correct conversion (no SQL injections!)
+        cur.execute(
+            "INSERT INTO test (num, data) VALUES (%s, %s)",
+            (100, "abc'def"))
+
+        # Query the database and obtain data as Python objects.
+        cur.execute("SELECT * FROM test")
+        cur.fetchone()
+        # will return (1, 100, "abc'def")
+
+        # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
+        # of several records, or even iterate on the cursor
+        for record in cur:
+            print(record)
+
+        # Make the changes to the database persistent
+        conn.commit()
